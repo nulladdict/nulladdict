d3f04df809032cc1d21a9d4fd5a0127e03a7e007
nulladdict <nulladdict@ya.ru>
2021-05-15 20:52:31 +0500

    chore: init svelte-kit template

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to my personal site</h1>
+<p>
+  There's nothing of value here at the moment, but you can check out my <a
+    href="https://github.com/nulladdict">github</a
+  > in the meantime
+</p>
