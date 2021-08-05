e70757bc57d8c8633ff8ca31ac706e778b4ccfa9
nulladdict <nulladdict@ya.ru>
2021-08-05 23:12:17 +0500

    style: use Inter font

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,8 @@
 <h1>Welcome to my personal site</h1>
 <p>
-  There's nothing of value here at the moment, but you can check out my <a
-    href="https://github.com/nulladdict">github</a
-  > in the meantime
+  There's nothing of value here <em>at the moment</em>, but you can check out my
+  <strong>
+    <a href="https://github.com/nulladdict">github</a>
+  </strong>
+  in the meantime.
 </p>
