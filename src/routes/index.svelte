624107847ebc49d8b7e442635bda00c6365a30b7
nulladdict <nulladdicted@gmail.com>
2021-08-09 00:26:17 +0500

    feat: add brief introduction

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,56 @@
-<h1>Welcome to my personal site</h1>
-<p>
-  There's nothing of value here <em>at the moment</em>, but you can check out my
-  <strong>
-    <a href="https://github.com/nulladdict">github</a>
-  </strong>
-  in the meantime.
-</p>
+<article>
+  <header>Hi!</header>
+  <h1>I'm Roman.</h1>
+  <aside>
+    Also known as <em>@nulladdict</em> on the web
+  </aside>
+  <main>
+    <p>I am a frontend developer, we(e)b enthusiast, and music consumer.</p>
+    <p>I also teach at university and occasionally contribute to open source.</p>
+    <p>
+      If you want to reach out, throw me an <a href="mailto:nulladdicted@gmail.com">e-mail</a>,
+      check my
+      <a href="https://github.com/nulladdict">github</a>, or hit me up on
+      <a href="https://twitter.com/nulladdict">twitter</a>.
+    </p>
+  </main>
+</article>
+
+<style>
+  article {
+    max-width: 40rem;
+    margin: 8vh 2vw;
+  }
+
+  header {
+    font-size: 2rem;
+    font-weight: 500;
+  }
+
+  h1 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    margin: 0.5rem 0;
+  }
+
+  aside {
+    font-weight: 300;
+  }
+
+  aside em {
+    font-weight: 450;
+    font-style: normal;
+  }
+
+  main {
+    margin: 1rem 0;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  p + p {
+    margin-top: 0.5rem;
+  }
+</style>
