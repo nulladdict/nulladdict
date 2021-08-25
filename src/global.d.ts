f5a1e77aa7a5b2422c86f0c89a43baa637646110
nulladdict <nulladdicted@gmail.com>
2021-08-25 21:12:16 +0500

    feat: add empty paint worklet

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="@sveltejs/kit" />
+
+interface PaintRenderingContext2D
+  extends CanvasCompositing,
+    CanvasDrawImage,
+    CanvasDrawPath,
+    CanvasFillStrokeStyles,
+    CanvasImageSmoothing,
+    CanvasPath,
+    CanvasPathDrawingStyles,
+    CanvasRect,
+    CanvasShadowStyles,
+    CanvasState,
+    CanvasTransform {}
+
+interface PaintSize {
+  readonly width: number;
+  readonly height: number;
+}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import "../app.css";
+  import workletUrl from "$lib/polka.js?url";
+  const tag = "script";
+  const register = `<${tag} type="module">if("paintWorklet" in CSS){CSS.paintWorklet.addModule("${workletUrl}")}</${tag}>`;
 </script>
 
 <slot />
+{@html register}
