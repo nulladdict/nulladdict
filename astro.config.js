37eb121a9e572ac04285f2c2ac23c9fcb9aa82ec
nulladdict <nulladdicted@gmail.com>
2025-08-17 16:13:20 +0200

    build: housekeeping

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ config: { applyBaseStyles: false } })],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
