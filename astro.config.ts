8d5ed2742f6a2480c37c7ee4b6dec67b890a48eb
nulladdict <nulladdicted@gmail.com>
2025-10-21 09:44:44 +0200

    chore: housekeeping

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    // @ts-expect-error: astro is still on vite 6
     plugins: [tailwindcss()],
   },
 });
